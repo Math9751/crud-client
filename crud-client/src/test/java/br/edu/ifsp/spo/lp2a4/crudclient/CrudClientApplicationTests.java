336aa286ca7a5e09eca8cfb7ac6be22d627da436
lukas <lukas@DESKTOP-QMTJSJF>
2022-11-30 00:16:42 -0300

    Update

---
+++ crud-client/src/test/java/br/edu/ifsp/spo/lp2a4/crudclient/CrudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.spo.lp2a4.crudclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
